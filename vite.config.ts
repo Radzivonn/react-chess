b15d96fbbd84f59a7d98d4206605a082e799709f
Rodion <rodionserez@gmail.com>
2024-11-13 23:59:16 +0300

    init: init base project

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+// import svgr from 'vite-plugin-svgr';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      API: '/src/API',
+      assets: '/src/assets',
+      components: '/src/components',
+      hocs: '/src/hocs',
+      hooks: '/src/hooks',
+      modules: '/src/modules',
+      pages: '/src/pages',
+      router: '/src/router',
+      store: '/src/store',
+      helpers: '/src/helpers',
+      styles: '/src/styles',
+      types: '/src/types',
+      validations: '/src/validations',
+      mocks: '/src/mocks',
+      tests: '/src/tests',
+    },
+  },
+});
